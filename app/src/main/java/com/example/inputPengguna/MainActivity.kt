ea5627d179b914f905d17f69b7a82923c02c0caf
RafaPramudhita <rafapram360@gmail.com>
2025-10-30 14:55:31 +0700

    16.menambahkan main activity

--- app/src/main/java/com/example/inputPengguna/MainActivity.kt
+++ app/src/main/java/com/example/inputPengguna/MainActivity.kt
@@ -1,2 +1,30 @@
+package com.example.inputPengguna
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.inputPengguna.ui.theme.InputPenggunaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InputPenggunaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    FormDataDiri(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
 
