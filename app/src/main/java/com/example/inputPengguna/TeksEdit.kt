60a54feee6f375cb4bca8deeec56c2b2d3447d4c
RafaPramudhita <rafapram360@gmail.com>
2025-10-30 14:52:28 +0700

    11.feat(input): tambah OutlinedTextField untuk input alamat

--- app/src/main/java/com/example/inputPengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputPengguna/TeksEdit.kt
@@ -75,7 +75,15 @@ fun FormDataDiri(modifier: Modifier) {
             }
         }
 
-
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(width = 250.dp),
+            label = { Text(text = "Alamat Lengakap") },
+            onValueChange = {
+                textAlamat = it
+            }
+        )
 
 
 
